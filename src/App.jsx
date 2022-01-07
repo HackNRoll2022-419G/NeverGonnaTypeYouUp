2b7fc02df2943b97952adb7e0905f853c4e7402c
richwill28 <e0550368@u.nus.edu>
2022-01-08 02:29:16 +0800

    Implement typing feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
-
 import './App.css';
 
 function App() {
   return (
     <div className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white grid grid-rows-6 grid-cols-1 justify-items-center min-h-screen">
-      <div className="border-2 border-rose-500 row-span-1 max-w-screen-lg"><Header /></div>
-      <div className="border-2 border-rose-500 row-span-4 max-w-screen-lg"><Body /></div>
-      <div className="border-2 border-rose-500 row-span-1 max-w-screen-lg"><Footer /></div>
+      <div className="border-2 border-rose-500 row-span-1 max-w-screen-lg">
+        <Header />
+      </div>
+      <div className="border-2 border-rose-500 row-span-4 max-w-screen-lg">
+        <Body />
+      </div>
+      <div className="border-2 border-rose-500 row-span-1 max-w-screen-lg">
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,82 @@
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from 'react';
+import getQuote from '../api/getQuote';
+
+const DEFAULT_QUOTE =
+  'Never gonna give you up. Never gonna let you down. Never gonna run around and desert you.';
+const DEFAULT_AUTHOR = 'Rick Astley';
+
 function Body() {
+  const [quote, setQuote] = useState(DEFAULT_QUOTE);
+  const [author, setAuthor] = useState(DEFAULT_AUTHOR);
+
+  const [letters, setLetters] = useState(DEFAULT_QUOTE.split(''));
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(async () => {
+    try {
+      const res = await getQuote();
+      setQuote(res.content);
+      setAuthor(res.author);
+      setLetters(res.content.split(''));
+    } catch (err) {
+      setQuote(DEFAULT_QUOTE);
+      setAuthor(DEFAULT_AUTHOR);
+      setLetters(DEFAULT_QUOTE.split(''));
+      console.log('Fail to fetch data');
+    } finally {
+      setActiveIndex(0);
+    }
+  }, []);
+
+  const [key, setKey] = useState('');
+
+  const [isLetterCorrect, setIsLetterCorrect] = useState([]);
+
+  const handleKeydown = (e) => {
+    if (e.key.length === 1) {
+      setKey(e.key);
+
+      if (e.key === letters[activeIndex]) {
+        setIsLetterCorrect([...isLetterCorrect, true]);
+      } else {
+        setIsLetterCorrect([...isLetterCorrect, false]);
+      }
+      setActiveIndex(activeIndex + 1);
+    } else if (e.key === 'Backspace') {
+      if (activeIndex > 0) {
+        setIsLetterCorrect(
+          isLetterCorrect.slice(0, isLetterCorrect.length - 1)
+        );
+        setActiveIndex(activeIndex - 1);
+      }
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeydown);
+
+    return function cleanup() {
+      document.removeEventListener('keydown', handleKeydown);
+    };
+  }, [handleKeydown]);
+
   return (
     <div className="text-justify">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      {quote.split('').map((letter, letterIndex) => (
+        <span
+          className={
+            // eslint-disable-next-line no-nested-ternary
+            isLetterCorrect[letterIndex] === true
+              ? 'font-bold text-sky-500'
+              : isLetterCorrect[letterIndex] === false
+              ? 'font-bold text-rose-500'
+              : ''
+          }
+        >
+          {letter}
+        </span>
+      ))}
     </div>
   );
 }
