d0fb9772abb1cb88cda227b871051cf28c318c37
richwill28 <e0550368@u.nus.edu>
2022-01-08 06:14:53 +0800

    Add custom blink animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blink: 'blink 1s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: 0 },
+        },
+      },
+    },
   },
   plugins: [],
   darkMode: 'class',
